ff0ee1b2bdc66f60b037285052550d9ee809cba3
Tero Björkman <silver_howlborne@hotmail.com>
2016-05-27 12:14:00 +0300

    A new and bright beginning

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        UserControl1 dude;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            dude = new UserControl1();
+            Canvas.SetTop(dude, dude.LocationY);
+            Canvas.SetLeft(dude, dude.LocationX);
+            Debug.WriteLine(dude.LocationX + " " + dude.LocationY);
+            can.Children.Add(dude);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dude.LocationX++;
+            dude.LocationY++;
+            dude.UpdatePosition();
+            //Canvas.SetTop(dude, dude.LocationY);
+            //Canvas.SetLeft(dude, dude.LocationX);
+            Debug.WriteLine(dude.LocationX + " " + dude.LocationY);
+        }
+        /*private void button_Click(object sender, RoutedEventArgs e)
+{
+   //dude.UpdatePosition();
+   Canvas.SetTop(dude, dude.LocationY);
+   Canvas.SetLeft(dude, dude.LocationX);
+   Debug.WriteLine(dude.LocationX + " " + dude.LocationY);
+   can.Children.Add(dude);
+}*/
+    }
+}
---
+++ WpfApplication1/WpfApplication1/UserControl1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+
+        public UserControl1()
+        {
+            InitializeComponent();
+            LocationX = 10;
+            LocationY = 20;
+            Width = 50;
+            Height = 50;
+        }
+
+        public void UpdatePosition()
+        {
+            Canvas.SetLeft (this, LocationX);
+            Canvas.SetTop (this, LocationY);
+        }
+    }
+}
