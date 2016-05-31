00589bee3b6f9ee334d679ace3cb81ae3a5f1fca
Tero Björkman <silver_howlborne@hotmail.com>
2016-05-31 19:01:32 +0300

    Works in UWP now. Movement still a little clunky

---
+++ BP2/BP2/MainControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BP2
+{
+    /// <summary>
+    /// Interaction logic for MainControl.xaml
+    /// </summary>
+    public partial class MainControl : UserControl
+    {
+        public MainControl()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            GameObject.Boulder boulder = new GameObject.Boulder();
+            boulder.LocationX = 20;
+            boulder.LocationY = 50;
+            MyCanvas.Children.Add(boulder);
+            boulder.UpdatePosition();
+            */
+
+            ((MainWindow)Application.Current.MainWindow).GameWindow();
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
